653b20ba4e92b636aa9704db9d2e7b185c337d43
Dinesh Manajipet <saidinesh5@gmail.com>
2021-05-05 19:15:09 +0530

    feat: Add a Linux x86_64 AppImage target

--- docker/build-containers.sh
+++ docker/build-containers.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 docker build -f Dockerfile.mingw32 -t qt5-build-environment-mingw32 .
+docker build -f Dockerfile.linux64 -t qt5-build-environment-linux64 .
 
 docker tag qt5-build-environment-mingw32 docker.pkg.github.com/saidinesh5/qt5-starter-template/qt5-build-environment-mingw32
+docker tag qt5-build-environment-linux64 docker.pkg.github.com/saidinesh5/qt5-starter-template/qt5-build-environment-linux64
 
--- run-in-qt-build-environment.sh
+++ run-in-qt-build-environment.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
 TARGET=${1:-build_mingw32}
+WORKDIR=/workdir
 
 if [[ `basename $TARGET` == build_mingw32 ]]; then
-    WORKDIR=/workdir
     CONTAINER=${CONTAINER:-docker.pkg.github.com/saidinesh5/qt5-starter-template/qt5-build-environment-mingw32}
+elif [[ `basename $TARGET` == build_linux64 ]]; then
+    CONTAINER=${CONTAINER:-docker.pkg.github.com/saidinesh5/qt5-starter-template/qt5-build-environment-linux64}
 else
     echo "Unknown target!"
     exit -1
